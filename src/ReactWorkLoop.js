e27e55208a0a9edd836df57c0bb374bed4eccd22
aaron <562540603@qq.com>
2023-10-07 21:22:16 +0800

    fiber的构建与任务执行

---
+++ src/ReactWorkLoop.js
@@ -0,0 +1,31 @@
+import { updateClassComponent, updateFunctionComponent, updateHostComponent } from "./ReactReconciler";
+import { isFn, isStr } from "./utils";
+
+let wip = null; // work in progress
+
+export function performUnitWork() {
+  // todo 1. 执行当前任务wip
+  // 判断wip是什么类型的组件
+  const { type } = wip;
+  if (isStr(type)) {
+    // 原生标签
+    updateHostComponent(wip)
+  } else if (isFn(wip)) {
+    type.prototype.isReactComponent ? updateClassComponent(wip) : updateFunctionComponent(wip);
+  }
+  // todo 2. 更新wip
+  // 深度优先遍历
+  if (wip.child) {
+    wip = wip.child;
+    return;
+  }
+  let next = wip;
+  while (next) {
+    if (next.sibling) {
+      wip = next.sibling;
+      return;
+    }
+    next = next.return;
+  }
+  wip = null;
+}
