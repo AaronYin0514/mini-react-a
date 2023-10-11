d4bbe5587d226ea375cc749ca8d767aca2ed7cdd
aaron <562540603@qq.com>
2023-10-11 20:36:12 +0800

    修改目录

--- src/ReactFiberReconciler.js
+++ src/ReactFiberReconciler.js
@@ -1,6 +1,6 @@
-import createFiber from "./ReactFiber";
-import { Update, isArray, isStringOrNumber, updateNode } from "./utils";
-import {renderWithHooks} from './hooks'
+import { updateNode } from "./utils";
+import { renderWithHooks } from './hooks'
+import { reconcileChildren } from "./ReactChildFiber";
 
 export function updateHostComponent(wip) {
   if (!wip.stateNode) {
@@ -32,63 +32,4 @@ export function updateFragmentComponent(wip) {
 
 export function updateHostTextComponent(wip) {
   wip.stateNode = document.createTextNode(wip.props.children);
-}
-
-function deleteChild(returnFiber, childToDelete) {
-  const deletions = returnFiber.deletions;
-  if (deletions) {
-    deletions.push(childToDelete);
-  } else {
-    returnFiber.deletions = [childToDelete];
-  }
-}
-
-// 协调（diff）
-function reconcileChildren(wip, children) {
-  if (isStringOrNumber(children)) {
-    return;
-  }
-  const newChildren = isArray(children) ? children : [children];
-  // oldfiber的头结点
-  let oldFiber = wip.alternate?.child;
-  let previousNewFiber = null; // 记录上一次的fiber
-  for (let i = 0; i < newChildren.length; i++) {
-    const newChild = newChildren[i];
-    if (newChild == null) {
-      continue;
-    }
-    const newFiber = createFiber(newChild, wip);
-    const same = sameNode(oldFiber, newFiber);
-
-    if (same) {
-      Object.assign(newFiber, {
-        stateNode: oldFiber.stateNode,
-        alternate: oldFiber,
-        flags: Update
-      })
-    }
-
-    if (!same && oldFiber) {
-      // 删除节点
-      deleteChild(wip, oldFiber);
-    }
-
-    if (oldFiber) {
-      oldFiber = oldFiber.sibling;
-    }
-
-    if (previousNewFiber === null) {
-      // head node
-      wip.child = newFiber;
-    } else {
-      previousNewFiber.sibling = newFiber;
-    }
-
-    previousNewFiber = newFiber;
-  }
-}
-
-// 节点复用的条件：1. 同一层级下 2. 类型相同 3. key相同
-function sameNode(a, b) {
-  return a && b && a.type === b.type && a.key === b.key;
 }
